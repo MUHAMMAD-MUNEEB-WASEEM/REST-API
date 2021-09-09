f905b31a59e4e7f3e11c62faf800faec22e682c2
Muhammad Muneeb Waseem <muneebwaseem78@gmail.com>
2021-09-09 20:24:32 +0500

    get and post method

---
+++ nodejs-rest-shop/api/routes/products.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const router = express.Router(); //to create routes
+
+router.get('/', (req, res, next)=>{
+    res.status(200).json({
+        message: "Handling GET requests to /products"
+    })
+
+})
+
+router.post('/', (req, res, next)=>{
+    res.status(200).json({
+        message: "Handling POST requests to /products"
+    })
+
+})
+
+module.exports = router;
--- nodejs-rest-shop/app.js
+++ nodejs-rest-shop/app.js
@@ -2,13 +2,10 @@ const express = require('express')
 
 const app = express();
 
+const productRoutes = require('./api/routes/products')
+
 
 //middleware
-app.use((req, res, next)=>{
-    res.status(200).json({
-        message: 'IT works'
-    })
-    res.end()
-});
+app.use('/products', productRoutes);
 
 module.exports = app;
