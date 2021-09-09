a8ba49721fbd19327685ca5a4810fa2ed5044237
Muhammad Muneeb Waseem <muneebwaseem78@gmail.com>
2021-09-09 22:23:17 +0500

    update orders and products.js and add cors avoiding code in app.js

--- nodejs-rest-shop/app.js
+++ nodejs-rest-shop/app.js
@@ -6,10 +6,29 @@ const app = express();
 const productRoutes = require('./api/routes/products')
 const orderRoutes = require('./api/routes/orders')
 
+
+//middleware
+
 //morgan shows log in terminal or console
 app.use(morgan('dev'))
 
-//middleware
+//body parser which is now replaced by express is used to parse body of your choice
+app.use(express.urlencoded({extended: false}))//parse simple bodies of url encoded data
+app.use(express.json());//this is to extract json data and make it readable
+
+//Insurance to prevent cors error
+app.use((req, res, next)=>{
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+
+    if (req.method === 'OPTIONS'){
+        res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+        return res.status(200).json({});
+    }
+    next(); 
+})
+
+//middleware to handle routes
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
 
