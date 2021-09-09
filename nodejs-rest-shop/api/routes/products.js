3b99592d9f06386e01ddad392c7a88141347fba7
Muhammad Muneeb Waseem <muneebwaseem78@gmail.com>
2021-09-09 20:58:04 +0500

    add another order route

--- nodejs-rest-shop/api/routes/products.js
+++ nodejs-rest-shop/api/routes/products.js
@@ -9,7 +9,7 @@ router.get('/', (req, res, next)=>{
 })
 
 router.post('/', (req, res, next)=>{
-    res.status(200).json({
+    res.status(201).json({
         message: "Handling POST requests to /products"
     })
 
