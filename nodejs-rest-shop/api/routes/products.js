3984b7ad80f860943fce489ae35fc36ce4e640d0
Muhammad Muneeb Waseem <muneebwaseem78@gmail.com>
2021-09-11 10:18:52 +0500

    update post route with storing files functionality

--- nodejs-rest-shop/api/routes/products.js
+++ nodejs-rest-shop/api/routes/products.js
@@ -1,6 +1,50 @@
 const express = require('express')
 const router = express.Router(); //to create routes
 const mongoose = require('mongoose')
+const multer = require('multer')
+
+//Below work is to enhance image experience with good filename and readable, limits, filters
+
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+      cb(null, './uploads');
+    },
+    filename: function(req, file, cb) {
+        cb(null, Date.now() + file.originalname);
+    }
+  });
+
+const fileFilter = (req, file, cb) => {
+
+    //reject or accept file on some conditions
+
+    if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png' || file.mimetype === 'image/jpg'){
+        
+    //accepting or storing file
+
+    cb(null, true);
+    
+    }else {
+
+    //rejecting
+    cb(null,false);
+    }
+}
+
+//main code combining all above to upload image
+const upload = multer({
+
+    storage: storage,
+    limits: {
+    fileSize: 1024 * 2024 * 5 //bytes, 5 MB
+    },
+    fileFilter: fileFilter
+    
+});//folder where multer uploads all files
+
+//This upload will be used to upload images in post route with upload.single
+
+//starting of routes
 
 const Product = require('../Schema/product');
 
@@ -40,13 +84,15 @@ router.get('/', (req, res, next)=>{
            })
 
 })
-
-router.post('/', (req, res, next)=>{
+//updating with upload.single for image or file
+router.post('/', upload.single('productImage'), (req, res, next)=>{
     // const product = {
     //     name: req.body.name,
     //     price: req.body.price
     // }
 
+    console.log(req.file)//available for upload.single
+    
     const product = new Product({
         _id: new mongoose.Types.ObjectId(),
         name: req.body.name,
