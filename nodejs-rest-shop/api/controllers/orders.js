8fca5e646b0165dc238bebe6d9bc6e8d955e0aa2
Muhammad Muneeb Waseem <muneebwaseem78@gmail.com>
2021-09-12 12:00:31 +0500

    clean order routes by copying all login in order controllers

---
+++ nodejs-rest-shop/api/controllers/orders.js
@@ -0,0 +1,124 @@
+const Order = require('../Schema/order');
+const Product = require('../Schema/product')
+const mongoose = require('mongoose');
+
+
+exports.orders_get_all = (req, res, next)=>{
+    Order.find()
+        .select("product quantity _id")
+        .populate('product', 'name')//name of ref property and selecting required fields , incase of relation, otherwise not used
+        .exec()
+        .then(docs => {
+            const response = {
+                count : docs.length,
+                order: docs.map(doc => {
+                    return {
+                        _id : doc._id,
+                        product: doc.product,
+                        quantity: doc.quantity,
+                        request : {
+                            type: 'GET',
+                            url : 'http://localhost:3000/orders/' + doc._id
+                        }
+                    }
+                })
+            }
+            res.status(200).json(response)
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: err
+            })
+        })
+}
+
+exports.order_create_order =  (req, res, next)=>{
+    // const order = {
+    //     productId: req.body.productId,
+    //     quantity: req.body.quantity
+    // }
+    Product.findById(req.body.productId)
+            .then(product=>{
+
+                if (!product){
+                    return res.status(404).json({
+                        message: "Product not found"
+                    })
+                }
+
+                const order = new Order({
+                    _id: mongoose.Types.ObjectId(),
+                    quantity: req.body.quantity,
+                    product: req.body.productId
+                });
+            
+                return order.save()
+            }).then(result => {
+                        const response = {
+                            message: 'Order Stored!',
+                            createdOrder: {
+                                _id: result._id,
+                                product: result.product,
+                                quantity: result.quantity 
+                            },
+                            request: {
+                            type: 'GET',
+                            url: 'http://localhost:3000/orders/' + result._id
+                            }
+                        }
+                        res.status(201).json(response);
+                    })
+                    .catch(err=>{
+                        console.log(err);
+                        res.status(500).json({
+                            error: err
+                        })
+                    })
+
+}
+
+exports.order_get_order = (req, res, next)=>{
+    Order.findById(req.params.orderId)
+         .select('-__v')//-ve sign to remove this __v field
+         .populate('product')//name of ref property and selecting all fields , incase of relation, otherwise not used
+         .exec()
+         .then(order =>{
+            if (!order){
+                res.status(404).json({
+                    message: "Order not found"
+                })
+            }
+             res.status(200).json({
+                 order: order,
+                 request: {
+                     type: 'GET',
+                     url: 'http://localhost:3000/orders'
+                 }
+             })
+         })
+         .catch(err => {
+             res.status(500).json({
+                 error:err
+             })
+         })
+}
+
+exports.orders_delete_order = (req, res, next)=>{
+    Order.remove({_id: req.params.orderId})
+        .exec()
+        .then(order => {
+            res.status(200).json({
+                message: "Order Deleted",
+                request: {
+                    type: 'POST',
+                    url: 'http://localhost:3000/orders',
+                    body: { productId : 'ID', quantity: "Number"},
+                }
+            })
+        })
+        .catch(err => {
+            res.status(500).json({
+                error:err
+         });
+    })
+}
--- nodejs-rest-shop/api/routes/orders.js
+++ nodejs-rest-shop/api/routes/orders.js
@@ -1,131 +1,21 @@
 const express = require('express')
 const router = express.Router(); //to create routes
-const mongoose = require('mongoose');
 
-const Order = require('../Schema/order');
-const Product = require('../Schema/product');
 const checkAuth  = require('../auth/check-auth')
 
-router.get('/', checkAuth, (req, res, next)=>{
-    Order.find()
-        .select("product quantity _id")
-        .populate('product', 'name')//name of ref property and selecting required fields , incase of relation, otherwise not used
-        .exec()
-        .then(docs => {
-            const response = {
-                count : docs.length,
-                order: docs.map(doc => {
-                    return {
-                        _id : doc._id,
-                        product: doc.product,
-                        quantity: doc.quantity,
-                        request : {
-                            type: 'GET',
-                            url : 'http://localhost:3000/orders/' + doc._id
-                        }
-                    }
-                })
-            }
-            res.status(200).json(response)
-        })
-        .catch(err => {
-            res.status(500).json({
-                error: err
-            })
-        })
-})
+//controllers
+const OrdersController = require('../controllers/orders');
 
-router.post('/', checkAuth, (req, res, next)=>{
-    // const order = {
-    //     productId: req.body.productId,
-    //     quantity: req.body.quantity
-    // }
-    Product.findById(req.body.productId)
-            .then(product=>{
 
-                if (!product){
-                    return res.status(404).json({
-                        message: "Product not found"
-                    })
-                }
+//routes
+router.get('/', checkAuth, OrdersController.orders_get_all)
 
-                const order = new Order({
-                    _id: mongoose.Types.ObjectId(),
-                    quantity: req.body.quantity,
-                    product: req.body.productId
-                });
-            
-                return order.save()
-            }).then(result => {
-                        const response = {
-                            message: 'Order Stored!',
-                            createdOrder: {
-                                _id: result._id,
-                                product: result.product,
-                                quantity: result.quantity 
-                            },
-                            request: {
-                            type: 'GET',
-                            url: 'http://localhost:3000/orders/' + result._id
-                            }
-                        }
-                        res.status(201).json(response);
-                    })
-                    .catch(err=>{
-                        console.log(err);
-                        res.status(500).json({
-                            error: err
-                        })
-                    })
-
-})
+router.post('/', checkAuth, OrdersController.order_create_order)
 
 //for particular order in orders
-router.get('/:orderId', checkAuth, (req, res, next)=>{
-    Order.findById(req.params.orderId)
-         .select('-__v')//-ve sign to remove this __v field
-         .populate('product')//name of ref property and selecting all fields , incase of relation, otherwise not used
-         .exec()
-         .then(order =>{
-            if (!order){
-                res.status(404).json({
-                    message: "Order not found"
-                })
-            }
-             res.status(200).json({
-                 order: order,
-                 request: {
-                     type: 'GET',
-                     url: 'http://localhost:3000/orders'
-                 }
-             })
-         })
-         .catch(err => {
-             res.status(500).json({
-                 error:err
-             })
-         })
-})
+router.get('/:orderId', checkAuth, OrdersController.order_get_order)
 
 
-router.delete('/:orderId', checkAuth, (req, res, next)=>{
-    Order.remove({_id: req.params.orderId})
-        .exec()
-        .then(order => {
-            res.status(200).json({
-                message: "Order Deleted",
-                request: {
-                    type: 'POST',
-                    url: 'http://localhost:3000/orders',
-                    body: { productId : 'ID', quantity: "Number"},
-                }
-            })
-        })
-        .catch(err => {
-            res.status(500).json({
-                error:err
-         });
-    })
-});
+router.delete('/:orderId', checkAuth, OrdersController.orders_delete_order);
 
 module.exports = router;
